59e3d2002540474f216a9e7c4856588cf2283c41
Ruchika <91602013+Ruchthedarkshadow@users.noreply.github.com>
2022-02-11 16:43:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,74 @@
-import cv2
-from matplotlib import pyplot as plt
-  
-# Opening image
-img = cv2.imread("image.jpg")
-  
-# OpenCV opens images as BRG 
-# but we want it as RGB We'll 
-# also need a grayscale version
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  
-  
-# Use minSize because for not 
-# bothering with extra-small 
-# dots that would look like STOP signs
-stop_data = cv2.CascadeClassifier('stop_data.xml')
-  
-found = stop_data.detectMultiScale(img_gray, 
-                                   minSize =(20, 20))
-  
-# Don't do anything if there's 
-# no sign
-amount_found = len(found)
-  
-if amount_found != 0:
-      
-    # There may be more than one
-    # sign in the image
-    for (x, y, width, height) in found:
-          
-        # We draw a green rectangle around
-        # every recognized sign
-        cv2.rectangle(img_rgb, (x, y), 
-                      (x + height, y + width), 
-                      (0, 255, 0), 5)
-          
-# Creates the environment of 
-# the picture and shows it
-plt.subplot(1, 1, 1)
-plt.imshow(img_rgb)
-plt.show()
+import cv2 # Computer vision library
+ 
+# Read the color image
+image = cv2.imread("/home/kkr/Pictures/PILE_OF_CLOTHES/1.jpg")
+ 
+# Make a copy
+new_image = image.copy()
+ 
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+# Display the grayscale image
+cv2.waitKey(0) # Wait for keypress to continue
+cv2.destroyAllWindows() # Close windows
+ 
+# Convert the grayscale image to binary
+ret, binary = cv2.threshold(gray, 100, 255, 
+  cv2.THRESH_OTSU)
+ 
+# Display the binary image
+cv2.waitKey(0) # Wait for keypress to continue
+cv2.destroyAllWindows() # Close windows
+ 
+# To detect object contours, we want a black background and a white 
+# foreground, so we invert the image (i.e. 255 - pixel value)
+inverted_binary = ~binary
+cv2.waitKey(0) # Wait for keypress to continue
+cv2.destroyAllWindows() # Close windows
+ 
+# Find the contours on the inverted binary image, and store them in a list
+# Contours are drawn around white blobs.
+# hierarchy variable contains info on the relationship between the contours
+contours, hierarchy = cv2.findContours(inverted_binary,
+  cv2.RETR_TREE,
+  cv2.CHAIN_APPROX_SIMPLE)
+     
+# Draw the contours (in red) on the original image and display the result
+# Input color code is in BGR (blue, green, red) format
+# -1 means to draw all contours
+with_contours = cv2.drawContours(image, contours, -1,(255,0,255),3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+ 
+# Show the total number of contours that were detected
+print('Total number of contours detected: ' + str(len(contours)))
+ 
+# Draw just the first contour
+# The 0 means to draw the first contour
+'''first_contour = cv2.drawContours(new_image, contours, 0,(255,0,255),3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+ 
+# Draw a bounding box around the first contour
+# x is the starting x coordinate of the bounding box
+# y is the starting y coordinate of the bounding box
+# w is the width of the bounding box
+# h is the height of the bounding box
+x, y, w, h = cv2.boundingRect(contours[0])
+cv2.rectangle(first_contour,(x,y), (x+w,y+h), (255,0,0), 5)
+cv2.waitKey(0)
+cv2.destroyAllWindows()'''
+ 
+# Draw a bounding box around all contours
+for c in contours:
+  x, y, w, h = cv2.boundingRect(c)
+ 
+    # Make sure contour area is large enough
+  if (cv2.contourArea(c)) > 800:
+    cv2.rectangle(with_contours,(x,y), (x+w,y+h), (255,0,0), 5)
+         
+cv2.imshow('All contours with bounding box', with_contours)
+cv2.imwrite('/home/kkr/imag1.jpg', with_contours)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
